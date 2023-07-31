00c63e3099189865ee910798024e14ef057b9af4
HoangKhang5207 <hoangkhang16112003@gmail.com>
2023-07-31 07:42:40 +0700

    Lan thu 2 lam chuyen ay - quen roi

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -11,7 +11,10 @@ public class MathUtilLive {
 
         //MathUtil.getFactorial(-5); ném ngoại lệ thật rồi, ai biểu đưa cà chớn chi!!!!
 
+        //THÊM CODE SAU LẦN ĐẦU TIÊN LÀM CHUYỆN ẤY - CODE LÊN SERVER 7:40 AM 31/07/2023
+        System.out.println("expected: 1! = 1;   actual: " + MathUtil.getFactorial(1));
 
+        System.out.println("expected: 3! = 6;   actual: " + MathUtil.getFactorial(3));
 
         //kỹ thuật kiểm thử phần mềm: ước lượng xem giá trị trả về của hàm là gì
         //expected value: 5! = 120
